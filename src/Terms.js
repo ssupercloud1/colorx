58856356ff5c53448daf297886c845515c77a0d6
Bonface Juma <bonface.juma15@gmail.com>
2023-12-31 16:39:42 +0300

    sixth commit

--- src/Terms.js
+++ src/Terms.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Terms = () => {
   return (
-    <div className="terms-container" style={{ textAlign: 'justified', padding: '20px', margin: 'auto', border: '2px solid #ccc', borderRadius: '7px' }}>
+    <div className="terms-container" style={{ textAlign: 'justified', padding: '15px', margin: 'auto', border: '2px solid #ccc', borderRadius: '5px' }}>
       <h2 style={{textAlign: 'center'}}>Terms of Use</h2>
       <p>Please read these terms carefully before using Colorx. By accessing or using the service, you agree to be bound by these terms.</p>
       {/* Add your terms of use content here */}
