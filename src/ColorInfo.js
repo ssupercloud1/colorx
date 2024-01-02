21a636004573d2833619b238d9153b5fe05550a8
Bonface Juma <bonface.juma15@gmail.com>
2024-01-02 18:21:47 +0300

    modified display of color info

--- src/ColorInfo.js
+++ src/ColorInfo.js
@@ -55,9 +55,12 @@ const ColorInfo = () => {
 
   return (
     <div className="color-info-page-container" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: 'auto' }}>
-      <div className="color-info-container" style={{ width: '83.8%', maxWidth: '100%', margin: 'auto', alignItems: 'center', border: '1px solid #ccc', borderRadius: '15px', marginBottom: '20px' }}>
-        <h2 style={{ textAlign: 'center' }}>Color Info: Color Finder, Contrast Checker & Gradient</h2>
-		<h3 style={{ textAlign: 'center' }}>Color Finder</h3>
+      <h2 style={{ textAlign: 'center', padding: '10px', marginBottom: '20px', marginTop: '0px', background: '#031501', color: '#ffffff' }}>
+	  Color Info: Color Finder, Contrast Checker & Gradient
+	  </h2>
+
+      <div className="color-info-container" style={{ width: '83.8%', maxWidth: '100%', margin: 'auto', alignItems: 'center', border: '1px solid #ccc', borderRadius: '15px', height: '400px', marginBottom: '20px' }}>
+        <h3 style={{ textAlign: 'center' }}>Color Finder</h3>
         <p style={{ textAlign: 'center', marginBottom: '10px' }}>
           Explore the details of a color by entering its name or HEX code below.
         </p>
